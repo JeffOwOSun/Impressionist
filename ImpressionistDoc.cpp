da9d2759a3c265cf8ca114b041c8907fc553473c
JeffOwOSun <sunyushi9546@gmail.com>
2015-09-12 11:16:11 +0800

    Reverse some unwanted changes in ImpressionistDoc.cpp

--- ImpressionistDoc.cpp
+++ ImpressionistDoc.cpp
@@ -14,15 +14,17 @@
 
 // Include individual brush headers here.
 #include "PointBrush.h"
+#include "ScatteredPointBrush.h"
+#include "CircleBrush.h"
 #include "LineBrush.h"
 #include "ScatteredLineBrush.h"
 
 #define DESTROY(p)	{  if ((p)!=NULL) {delete [] p; p=NULL; } }
 
-ImpressionistDoc::ImpressionistDoc() 
+ImpressionistDoc::ImpressionistDoc()
 {
-	// Set NULL image name as init. 
-	m_imageName[0]	='\0';	
+	// Set NULL image name as init.
+	m_imageName[0]	='\0';
 
 	m_nWidth		= -1;
 	m_ucBitmap		= NULL;
@@ -36,15 +38,15 @@ ImpressionistDoc::ImpressionistDoc()
 	ImpBrush::c_pBrushes[BRUSH_POINTS]	= new PointBrush( this, "Points" );
 
 	// Note: You should implement these 5 brushes.  They are set the same (PointBrush) for now
-	ImpBrush::c_pBrushes[BRUSH_LINES]				
+	ImpBrush::c_pBrushes[BRUSH_LINES]
 		= new LineBrush( this, "Lines" );
-	ImpBrush::c_pBrushes[BRUSH_CIRCLES]				
-		= new PointBrush( this, "Circles" );
-	ImpBrush::c_pBrushes[BRUSH_SCATTERED_POINTS]	
-		= new PointBrush( this, "Scattered Points" );
-	ImpBrush::c_pBrushes[BRUSH_SCATTERED_LINES]		
+	ImpBrush::c_pBrushes[BRUSH_CIRCLES]
+		= new CircleBrush( this, "Circles" );
+	ImpBrush::c_pBrushes[BRUSH_SCATTERED_POINTS]
+		= new ScatteredPointBrush( this, "Scattered Points" );
+	ImpBrush::c_pBrushes[BRUSH_SCATTERED_LINES]
 		= new ScatteredLineBrush( this, "Scattered Lines" );
-	ImpBrush::c_pBrushes[BRUSH_SCATTERED_CIRCLES]	
+	ImpBrush::c_pBrushes[BRUSH_SCATTERED_CIRCLES]
 		= new PointBrush( this, "Scattered Circles" );
 
 	// make one of the brushes current
